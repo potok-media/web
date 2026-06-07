e75f6530d8f0c7bb2d380223d4538812b9b19c3a
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-06-07 13:49:18 +0300

    fix

--- src/utils/extensions/iframeHelper.ts
+++ src/utils/extensions/iframeHelper.ts
@@ -68,7 +68,8 @@ export const createIframeHtml = (
     </head>
     <body>
       <script type="module">
-        import("./${ext.manifest.entrypoint}?v=${ext.manifest.version || Date.now()}").catch(err => {
+        const entryUrl = new URL("./${ext.manifest.entrypoint}?v=${ext.manifest.version || Date.now()}", document.baseURI).href;
+        import(entryUrl).catch(err => {
           window.parent.postMessage({
             source: 'potok-plugin-sdk',
             action: 'SCRIPT_CRASH',
